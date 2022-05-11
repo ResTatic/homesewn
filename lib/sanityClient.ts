57dd93ee85d155b381678e40ea610c0d738ff938
Res Tatic <73036909+ResTatic@users.noreply.github.com>
2022-05-11 11:00:47 +0200

    use testing dataset

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -3,13 +3,13 @@ import sanityClient from '@sanity/client'
 export const liveClient = sanityClient({
   apiVersion: '2022-05-05',
   projectId: 'avejjwu6',
-  dataset: 'production',
+  dataset: 'testing',
   useCdn: false,
 })
 
 export const cdnClient = sanityClient({
   apiVersion: '2022-05-05',
   projectId: 'avejjwu6',
-  dataset: 'production',
+  dataset: 'testing',
   useCdn: true,
 })
